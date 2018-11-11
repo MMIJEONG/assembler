67f678b786a5baf8e6014ef603be1302fcdc5279
lmj <mijeong082191@gmail.com>
2018-11-12 01:12:35 +0900

    trans.c added2

--- myas.h
+++ myas.h
@@ -4,3 +4,4 @@
 
 // complete this header file
 int is_valid(char *op, char *args);
+int instr_trans(char *op, char *args, char* mcode);
--- trans.c
+++ trans.c
@@ -18,16 +18,50 @@ int instr_trans(char *op, char *args, char* mcode)
 	for(i = 1 ; i < strlen(args) ; i++)
 	{
 		if(args[i]==',')
+		{
+			if(term == '%')
 			{
-				if(term == '%')
+				if(args[i+1] == '%')
+					return "89";
+				else if(args[i+1] == '0')
+					return "a3";
+			}
+			else if(term=='0')
+			{
+				if(args[i-1]==')')
+					return "8b";
+				else if(args[i+1]=='%')
+					return "a1";
+			}
+			else if(term=='$')
+			{			
+				if(args[i+3]=='a')
+					return "b8";
+				else if(args[i+3]=='c')
+					return "b9";
+				else if(args[i+3]=='d')
 				{
-					if(args[i+1] == '%')
-						return "89";
-					else if(args[i+1] == '0')
-						return "a3";
+					if(args[a+4]=='x')
+						return "ba";
+					else if(args[a+4]=='i')
+						return "bf";
+					}
+				else if(args[i+3]=='b')
+				{
+					if(args[a+4]=='x')
+						return "bb";
+					else if(args[a+4]=='p')
+						return "bd";
 				}
-				else if(
-			}	
+				else if(args[i+3]=='s')
+				{
+					if(args[a+4]=='p')
+					return "bc";
+					else if(args[a+4]=='i')
+						return "be";
+				}	
+			}
+		}	
 	}
 	return 1;	
 }
