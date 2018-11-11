85141fbba965138cb31be59ac925a0c66da85c1f
lmj <mijeong082191@gmail.com>
2018-11-11 23:08:38 +0900

    check.c write

--- check.c
+++ check.c
@@ -1,15 +1,24 @@
 #include "myas.h"
+#include <string.h>
 
 int is_valid(char *op, char *args)
 {
-	//printf("if valid, return 1\n");
-	//printf("otherwise, return 0\n");
-
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /*  							*/
-	/********************************/
-	
-	return 1;
+	int i,c=0;
+	if(args[0] == '%')
+		c++;
+	for(i = 1 ; i < strlen(args) ; i++)
+	{
+		if(args[i] == ',')
+		{
+			if(args[i+1] == '%')
+			{
+				c++;
+				break;
+			}
+		}
+	}
+	if(c>=1)
+		return 1;
+	else
+		return 0;
 }
